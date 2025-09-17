3bd10f933e7fb77da5e2e2dd131ba29299384cef
TheBaytur <bayturcodding@gmail.com>
2025-09-18 02:28:52 +0800

    Text

--- lib/widgets/auth/auth_widget.dart
+++ lib/widgets/auth/auth_widget.dart
@@ -10,11 +10,30 @@ class AuthWidget extends StatefulWidget {
 class _AuthWidgetState extends State<AuthWidget> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: const Text('Login to your account', style: TextStyle(color: Colors.white),
+        title: const Text(
+          'Login to your account',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-      ),
+      body: ListView(children: [_HeaderWidget()]),
+    );
+  }
+}
+
+class _HeaderWidget extends StatelessWidget {
+  const _HeaderWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text(
+          'In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple. ',
+        ),
+        Text('If you signed up but didn\'t receive the verification email, click here to have it resent. '),
+      ],
     );
   }
 }
