b5a445130a69fb896a4ac4d59321bf52ee98cbc0
TheBaytur <bayturcodding@gmail.com>
2025-09-17 22:05:06 +0800

    debugBanner

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      theme: ThemeData( 
+        primarySwatch: Colors.blue
       ),
-      home: Text('My movie application'),
+      home: Text('Start'),
     );
   }
 }
