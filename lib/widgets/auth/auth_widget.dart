21596e94fc1e1b1ed162971b3563b25a6434455e
TheBaytur <bayturcodding@gmail.com>
2025-09-18 22:58:52 +0800

    _FormWidget

--- lib/widgets/auth/auth_widget.dart
+++ lib/widgets/auth/auth_widget.dart
@@ -27,14 +27,75 @@ class _HeaderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return const Padding(
+      padding:  EdgeInsets.symmetric(horizontal: 20),
+      child:  Column(
+        children: [
+          SizedBox(height: 25),
+          _FormWidget(),
+          SizedBox(height: 25),
+          Text(
+            'In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple. ',
+            style: TextStyle(fontSize: 16, color: Colors.black54),
+          ),
+                  SizedBox(height: 25),
+          Text('If you signed up but didn\'t receive the verification email, click here to have it resent. ',
+          style: TextStyle(fontSize: 16, color: Colors.black54),
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
+
+class _FormWidget extends StatefulWidget {
+  const _FormWidget({super.key});
+
+  @override
+  State<_FormWidget> createState() => __FormWidgetState();
+}
+
+class __FormWidgetState extends State<_FormWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
       children: [
-        SizedBox(height: 25),
-        Text(
-          'In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple. ',
+        TextField(
+          decoration: InputDecoration(
+            labelText: 'Username',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
         ),
-                SizedBox(height: 25),
-        Text('If you signed up but didn\'t receive the verification email, click here to have it resent. '),
+        const SizedBox(height: 20),
+        TextField(
+          obscureText: true,
+          decoration: InputDecoration(
+            labelText: 'Password',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        SizedBox(
+          width: double.infinity,
+          height: 50,
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: const Text(
+              'Login',
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+        ),
       ],
     );
   }
