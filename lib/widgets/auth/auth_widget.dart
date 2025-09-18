5616c6756334fe2076fd36a09d82fffdb86a1edd
TheBaytur <bayturcodding@gmail.com>
2025-09-18 22:28:58 +0800

    2 Sized box 25

--- lib/widgets/auth/auth_widget.dart
+++ lib/widgets/auth/auth_widget.dart
@@ -33,6 +33,7 @@ class _HeaderWidget extends StatelessWidget {
         Text(
           'In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations you will need to login to your account. If you do not have an account, registering for an account is free and simple. ',
         ),
+                SizedBox(height: 25),
         Text('If you signed up but didn\'t receive the verification email, click here to have it resent. '),
       ],
     );
