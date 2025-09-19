b654d958d157ff4c3717512b369cacad4e03124a
TheBaytur <bayturcodding@gmail.com>
2025-09-20 00:55:18 +0800

    Button text color

--- lib/widgets/auth/auth_widget.dart
+++ lib/widgets/auth/auth_widget.dart
@@ -63,60 +63,55 @@ class __FormWidgetState extends State<_FormWidget> {
     color: Color(0xFF212519),
   );
   final textFieldDecoration = const InputDecoration(
-    contentPadding:  EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+    contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
   );
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          'Username',
-          style: style,
-        ),
+        Text('Username', style: style),
         TextField(
           decoration: InputDecoration(
-            
             border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-            
           ),
         ),
         const SizedBox(height: 20),
-        Text(
-          'Password',
-          style: style,
-          
-        ),
+        Text('Password', style: style),
         TextField(
           obscureText: true,
           decoration: InputDecoration(
-            
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10),
-            borderSide: const BorderSide(color: Colors.blue),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.blue),
             ),
           ),
         ),
-
 
         const SizedBox(height: 20),
         Row(
           children: [
             ElevatedButton(
-            onPressed: () {},
-            style: ButtonStyle(
-              backgroundColor: WidgetStateProperty.all(Colors.blue
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Colors.blue),
+              ),
+              child: const Text(
+                'Login',
+                style: TextStyle(fontSize: 18, color: Colors.amber),
               ),
             ),
-            child: const Text('Login', style: TextStyle(fontSize: 18)),
-          ),
-          const SizedBox(width: 20),
-          ElevatedButton(
-            onPressed: () {},
-            style: ButtonStyle(backgroundColor: WidgetStateProperty.all(Colors.blue)), 
-            child: const Text('Reset password', style: TextStyle(fontSize: 18)),
-          ),
-
-
+            const SizedBox(width: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Colors.blue),
+              ),
+              child: const Text(
+                'Reset password',
+                style: TextStyle(fontSize: 18, color: Colors.amber),
+              ),
+            ),
           ],
         ),
       ],
