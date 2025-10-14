9bbfeb3c68ecc331b60e3b88d2d24cb71dd59a1e
TheBaytur <bayturcodding@gmail.com>
2025-10-14 21:57:06 +0800

    14.10

--- lib/main.dart
+++ lib/main.dart
@@ -33,26 +33,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// class ExampleWidget extends StatefulWidget {
-//   const ExampleWidget({super.key});
 
-//   @override
-//   _MainScreenWidgetState createState() => _MainScreenWidgetState();
-// }
-
-// class _MainScreenWidgetState extends State<ExampleWidget> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('TMDB', style: TextStyle(color: Colors.white)),
-//       ),
-//       body: Center(
-//        child: ElevatedButton(
-//         onPressed: () {
-//           Navigator.of(context).pop();
-//         }, child: const Text('Press me')),
-//       )
-//     );
-//   }
-// }
