37603f7be07af1c9c9fcda3e8fd60e097cacc125
TheBaytur <bayturcodding@gmail.com>
2025-11-03 15:56:02 +0800

    Text

--- lib/widgets/movie_list/movie_list_widget.dart
+++ lib/widgets/movie_list/movie_list_widget.dart
@@ -10,14 +10,16 @@ class MovieListWidget extends StatelessWidget {
       itemCount: 10,
       itemExtent: 163,
       itemBuilder: (context, index) {
-        return const Padding(
-          padding:  EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
           child: ClipRRect(
             borderRadius: BorderRadius.all(Radius.circular(20)),
             child: DecoratedBox(
               decoration: BoxDecoration(
-                //color: Colors.white,
-                boxShadow: [
+                color: Colors.white,
+                border: Border.all(color: Colors.black12.withOpacity(0.2)),
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                boxShadow: const [
                   BoxShadow(
                     color: Colors.black12,
                     spreadRadius: 2,
@@ -26,9 +28,14 @@ class MovieListWidget extends StatelessWidget {
                   ),
                 ],
               ),
-              child: Row(
+              child: const Row(
                 children: [
-                  Image(image: AssetImage(AppImages.console)),],
+                  Image(image: AssetImage(AppImages.console)),
+                  Column (children: [
+                    Text('Movie Title'),
+                    Text('Movie Description'),
+                  ],),
+                ],
               ),
             ),
           ),
