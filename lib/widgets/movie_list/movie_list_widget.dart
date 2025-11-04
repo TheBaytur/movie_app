d73edd31bf8f1734b56412a1b5757d569d267a0d
TheBaytur <bayturcodding@gmail.com>
2025-11-04 15:18:39 +0800

    Inkwell fixed

--- lib/widgets/movie_list/movie_list_widget.dart
+++ lib/widgets/movie_list/movie_list_widget.dart
@@ -12,64 +12,72 @@ class MovieListWidget extends StatelessWidget {
       itemBuilder: (context, index) {
         return Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          child: ClipRRect(
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.black12.withOpacity(0.2)),
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black12,
-                    spreadRadius: 2,
-                    blurRadius: 8,
-                    offset: Offset(0, 5),
-                  ),
-                ],
-              ),
-              
-              child: InkWell(
-                onTap: () {
-                  print('Movie item tapped at index $index');
-                },
-                child: const Row(
-                  children: [
-                    Image(image: AssetImage(AppImages.console)),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(height: 16),
-                          Text(
-                            'Movie Title',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            'Movie Description',
-                            style: TextStyle(color: Colors.grey),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          SizedBox(height: 5),
-                          Text('2024-01-01', style: TextStyle(color: Colors.grey),),
-                          SizedBox(height: 5),
-                          Text(
-                            'Opisanie filma fdfjsjdfnsjdfnsdfnsijdfnsijdnfisjdnfisjdfnisjnfijs',
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ],
+          child: Stack(
+            children: [
+              Container(
+                
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.black12.withOpacity(0.2)),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black12,
+                        spreadRadius: 2,
+                        blurRadius: 8,
+                        offset: Offset(0, 5),
                       ),
-                    ),
-                    SizedBox(width: 5),
-                  ],
+                    ],
+                  ),
+                  
+                  child: const Row(
+                    children: [
+                      Image(image: AssetImage(AppImages.console)),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 16),
+                            Text(
+                              'Movie Title',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              'Movie Description',
+                              style: TextStyle(color: Colors.grey),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            SizedBox(height: 5),
+                            Text('2024-01-01', style: TextStyle(color: Colors.grey),),
+                            SizedBox(height: 5),
+                            Text(
+                              'Opisanie filma fdfjsjdfnsjdfnsdfnsijdfnsijdnfisjdnfisjdfnisjnfijs',
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 5),
+                    ],
+                  ),
                 ),
               ),
-            ),
+              Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  onTap: () {
+                    print ('Movie item tapped: $index');
+                  },
+                ),
+              ),
+            ],
           ),
         );
       },
