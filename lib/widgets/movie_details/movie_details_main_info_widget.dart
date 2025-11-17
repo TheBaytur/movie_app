850385027ec50dc088f663b037707a0492f31a9e
TheBaytur <bayturcodding@gmail.com>
2025-11-17 15:52:53 +0800

    Extract Widget

--- lib/widgets/movie_details/movie_details_main_info_widget.dart
+++ lib/widgets/movie_details/movie_details_main_info_widget.dart
@@ -2,17 +2,114 @@ import 'package:flutter/material.dart';
 import 'package:movie_app/resources/resources.dart';
 
 class MovieDetailsMainInfoWidget extends StatelessWidget {
-  const MovieDetailsMainInfoWidget({Key? key}) : super (key:key);
+  const MovieDetailsMainInfoWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children:  [
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
         _TopPosterWidget(),
         SizedBox(height: 20),
-        _MovieNameWidget(),
-        _SummaryWidget()
+        Padding(
+          padding: EdgeInsets.all(10.0),
+          child: _MovieNameWidget(),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 80),
+          child: _SummaryWidget(),
+        ),
+
+        Padding(
+          padding: EdgeInsets.all(10.0),
+          child: _OverviewWidget(),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10.0),
+          child: _DescriptionWidget(),
+        ),
+        SizedBox(height: 20),
+        Row(
+          children: [
+            Column(
+              children: [
+                Text(
+                  'Director',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'Director',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Text(
+                  'Director',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'Director',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ],
+    );
+  }
+}
+
+class _DescriptionWidget extends StatelessWidget {
+  const _DescriptionWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Cast out from his clan, a young Predator finds an unlikely ally in a damaged android and embarks on a treacherous journey in search of the ultimate adversary',
+    style: TextStyle(
+          fontSize: 13,
+          fontWeight: FontWeight.w600,
+          color: Colors.white,
+    ),
+    );
+  }
+}
+
+class _OverviewWidget extends StatelessWidget {
+  const _OverviewWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Overview',
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+        color: Colors.white,
+      ),
     );
   }
 }
@@ -22,11 +119,7 @@ class _TopPosterWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Stack(children: [
-
-        Image(image: AssetImage(AppImages.actor)),
-    ],
-    );
+    return const Stack(children: [Image(image: AssetImage(AppImages.actor))]);
   }
 }
 
@@ -41,7 +134,7 @@ class _MovieNameWidget extends StatelessWidget {
       text: const TextSpan(
         children: [
           TextSpan(
-            text: 'Inception ',
+            text: 'White Scar ',
             style: TextStyle(
               fontSize: 17,
               fontWeight: FontWeight.w600,
@@ -58,7 +151,6 @@ class _MovieNameWidget extends StatelessWidget {
           ),
         ],
       ),
-      
     );
   }
 }
@@ -68,6 +160,15 @@ class _SummaryWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Text(
+      ' R, PG * 11/05/2025 * (PH) *  Action, Science Fiction, Adventure 1h 47m',
+      maxLines: 3,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        color: Colors.white60,
+      ),
+    );
   }
 }
