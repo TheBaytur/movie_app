0eaf6aa156e13bedb6016e37094272cdc451d674
TheBaytur <bayturcodding@gmail.com>
2025-11-17 13:54:07 +0800

    17

--- lib/widgets/movie_details/movie_details_main_info_widget.dart
+++ lib/widgets/movie_details/movie_details_main_info_widget.dart
@@ -9,6 +9,8 @@ class MovieDetailsMainInfoWidget extends StatelessWidget {
     return const Column(
       children:  [
         _TopPosterWidget(),
+        SizedBox(height: 20),
+        _MovieNameWidget(),
       ],
     );
   }
@@ -20,11 +22,40 @@ class _TopPosterWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Stack(children: [
-       
-      
+
         Image(image: AssetImage(AppImages.actor)),
-        
     ],
+    );
+  }
+}
+
+class _MovieNameWidget extends StatelessWidget {
+  const _MovieNameWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: const TextSpan(
+        children: [
+          TextSpan(
+            text: 'Inception ',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
+          ),
+          TextSpan(
+            text: '(2010)',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w400,
+              color: Colors.black54,
+            ),
+          ),
+        ],
+      ),
+      
     );
   }
 }
