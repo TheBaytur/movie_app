d3e38f268b26f59f8fb3c849f147927d2de1103c
TheBaytur <bayturcodding@gmail.com>
2026-01-14 19:37:57 +0800

    auth_model

---
+++ lib/widgets/auth/auth_model.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AuthModel extends ChangeNotifier {}
+
+class AuthProvider extends InheritedNotifier {
+final AuthModel model;
+  const AuthProvider({
+    Key? key, 
+    required this.model,
+    required Widget child,
+    }) : super(
+      key: key,
+      child: child,
+      notifier: model
+      );
+
+  static AuthProvider? watch(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<AuthProvider>();
+  }
+
+  static AuthProvider? read(BuildContext context) {
+    final widget = context.getElementForInheritedWidgetOfExactType<AuthProvider>()?.widget;
+    return widget is AuthProvider ? widget : null;
+  }
+
+  
+}
+
+
