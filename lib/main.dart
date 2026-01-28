77148ec4087ad52f857468c84af00999c5cfb1e8
TheBaytur <bayturcodding@gmail.com>
2026-01-28 20:04:25 +0800

    main => my_app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:movie_app/main_screen/main_screen_widget.dart';
-import 'package:movie_app/ui/navigation/main_navigation.dart';
-import 'package:movie_app/widgets/auth/auth_model.dart';
-import 'package:movie_app/widgets/auth/auth_widget.dart';
-import 'package:movie_app/Theme/app_colors.dart';
-import 'package:movie_app/widgets/movie_details/movie_details_widget.dart';
+import 'package:movie_app/widgets/app/my_app.dart';
 
-// corrected ignore: use_key_in_widget_constructors
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+
+
+void main() {
+  const app = MyApp();
+  runApp(app);
 }
 
-class MyApp extends StatelessWidget {
-  static final mainNavigation = MainNavigation();
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: AppColors.mainDarkBlue,
-        ),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: AppColors.mainDarkBlue,
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.grey,
-        ),
-      ),
-      routes: mainNavigation.routes,
-      initialRoute: mainNavigation.initialRoute(false),
-      onGenerateRoute: mainNavigation.onGenerateRoute
-    );
-  }
-}
+
