51cb01308a318a82d3493d5cabf7962a1f76d8fc
TheBaytur <bayturcodding@gmail.com>
2026-02-09 21:52:55 +0800

    Provider fixed

--- lib/library/widgets/inherited/provider.dart
+++ lib/library/widgets/inherited/provider.dart
@@ -24,3 +24,38 @@ class NotifierProvider<Model extends ChangeNotifier> extends InheritedNotifier {
     return widget is NotifierProvider<Model> ? widget.model : null;
   }
 }
+
+class Provider<Model> extends InheritedWidget {
+  final Model model;
+  
+  const Provider({
+    Key? key, 
+    required this.model,
+   required Widget child
+   }) : super(
+    key: key, 
+    child: child);
+
+  
+
+  static Provider? of<Model>(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<Provider<Model>>();
+  }
+
+  static Model? watch<Model extends ChangeNotifier>(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<NotifierProvider<Model>>()
+    ?.model;
+  }
+
+  static Model? read<Model extends ChangeNotifier>(BuildContext context) {
+    final widget =
+        context.getElementForInheritedWidgetOfExactType<NotifierProvider<Model>>()?.widget;
+    return widget is NotifierProvider<Model> ? widget.model : null;
+  }
+
+  @override
+  bool updateShouldNotify( Provider oldWidget) {
+    return true;
+  }
+}
+  
